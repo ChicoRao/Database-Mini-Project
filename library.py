f005d8cee9cd92d3a5451de0481f1605155ab3a2
Nikola Maeda <nmaeda@sfu.ca>
2020-04-06 22:13:54 -0700

    Register implemented

--- library.py
+++ library.py
@@ -2,6 +2,21 @@ import sqlite3
 conn = sqlite3.connect('library.db')
 
 
+def makeSureNotNull(attribute_name):
+    while True:
+        attribute = input("Enter "+attribute_name+" : ")
+        if(attribute is None):
+            print("Attribute "+attribute_name+" cannot be NULL")
+        else:
+            return attribute
+
+def ifEmptySetToNULL(attribute_name):
+    attribute = input("Enter "+attribute_name+" : ")
+    if(attribute is None):
+        return "NULL"
+    else:
+        return attribute
+
 def displayHelp ():
     print("/finditem     : find item from the library")
     print("/borrow       : Borrow item from the library")
@@ -164,6 +179,95 @@ def returnItem(cursor):
     cursor.execute("UPDATE Items SET availability = 'available' WHERE id = "+item_id)
     conn.commit()
 
+def findevent(cursor):
+    attribute = input("Search by name, eid, type, description, room, audiences or eventDate? : ")
+    if(attribute == "name" or attribute == "type" or attribute == "description" or attribute == "audiences" or attribute == "room"):
+        search = input("Enter the "+ attribute +" you are searching for :")
+        cursor.execute("SELECT * FROM Event_Detail NATURAL JOIN Events WHERE "+attribute+" LIKE '%"+search+"%'")
+    elif(attribute == "eid"):
+        search = input("Enter the event ID : ")
+        cursor.execute("SELECT * FROM Event_Detail NATURAL JOIN Events WHERE eid = "+search)
+    elif(attribute == "eventDate"):
+        search = input("Enter the date (YYYY-MM-DD) : ")
+        cursor.execute("SELECT * FROM Event_Detail NATURAL JOIN Events WHERE eventDate = "+search)
+    else:
+        print("Invalid attribute name")
+        return
+    
+    result = cursor.fetchall()
+    if(len(result) == 0):
+        print("Event not found. ")
+    for row in result:
+        print("ID: "+str(row[5]))
+        print("Name: "+str(row[1]))
+        print("Type: "+str(row[2]))
+        print("Audience: "+str(row[3]))
+        print("Description: "+str(row[4]))
+        print("Room: "+str(row[6]))
+        print("Date: "+str(row[7]))
+        print("Time: "+str(row[8]))
+        print("--------------------")
+
+def donate(cursor):
+    with conn:
+        while not int(item) in range(1, 4):
+            item = input("How can I help you today? Here are the options: \n"
+                                "1. CD \n"
+                                "2. Magazines \n"
+                                "3. Scientific Journals \n"
+                                "4. Books \n"
+                                "Please enter the number corresponding to the item you want to donate. \n")
+
+        cursor.execute("SELECT MAX(id) FROM Items")
+        new_id = str(cursor.fetchone()+1)
+        if (item == 1):
+            ISRC = input("Enter ISRC of the CD :")
+            cursor.execute("SELECT * FROM CD_Detail WHERE ISRC = "+ISRC)
+            ISRC_result = curosr.fetchall()
+            
+        #     if(len(ISRC_result) != 0):
+
+
+
+        # elif(item == 2):
+        # elif(item == 3):
+        # elif(item == 4):
+        
+def register(cursor):
+    with conn:
+        cursor.execute("SELECT MAX(eid) FROM Events")
+        new_id = int(cursor.fetchone()[0])
+        
+        if(new_id is None):
+            new_id = 1
+        else:
+            new_id += 1
+        
+        event = input("Enter unique event type identification : ")
+        cursor.execute("SELECT event FROM Event_Detail WHERE event = "+ event)
+        if(cursor.fetchone() is None):
+            cursor.execute("SELECT MAX(event) FROM Event_Detail")
+            event = cursor.fetchone()
+            if(new_id is None):
+                event = 1
+            else :
+                event += 1
+            
+            name = makeSureNotNull("event name")
+            event_type = makeSureNotNull("event type")
+            audience = makeSureNotNull("audience")
+            description = ifEmptySetToNULL("description")
+            cursor.execute("INSERT INTO Event_Detail (event,name,type,audience,description) VALUES ("+str(event)+",'"+name+"','"+event_type+"','"+audience+"','"+description+"')")
+
+        room = makeSureNotNull("room")
+        eventDate = ifEmptySetToNULL("event date")
+        eventTime = ifEmptySetToNULL("event time")
+
+        cursor.execute("INSERT INTO Events (eid, event, room, eventDate, eventTime) VALUES ("+str(new_id)+","+str(event)+",'"+room+"','"+eventDate+"','"+eventTime+"')")
+        print("Event with event id "+str(new_id)+" has been registered.")
+        conn.commit()
+
+
 
 cursor = conn.cursor()
 while 1:
@@ -176,12 +280,11 @@ while 1:
         finditem(cursor)
     elif command == "/return":
         returnItem(cursor)
+    elif command == "/findevent":
+        findevent(cursor)
+    elif command == "/register":
+        register(cursor)
 
-    #else if command == "/donate":
-
-    # else if command == "/findevent":
-
-    # else if command == "/register":
 
     # else if command == "/volunteer":
 
